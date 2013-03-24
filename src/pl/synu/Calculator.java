b43be20ecf4ed3a46c546f7a4965a8dcd55a3c5f
synu <synu@wp.eu>
2013-03-24 23:19:24 +0100

    Repository init commit.

---
+++ src/pl/synu/Calculator.java
@@ -0,0 +1,21 @@
+package pl.synu;
+
+public class Calculator {
+	
+	public Calculator() {
+		
+	}
+	
+	public int multiply(int a, int b){
+		return a*b;
+	}
+	
+	public double divide(int a, int b){
+		return a/b;
+	}
+	
+	public static void main(String[] args){
+		new Calculator();
+	}
+
+}
---
+++ test/pl/synu/CalculatorTest.java
@@ -0,0 +1,32 @@
+package pl.synu;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	private static Calculator calc;
+	
+	@BeforeClass
+	public static void init(){
+		calc = new Calculator();
+	}
+
+	@Test
+	public void testMultiply(){
+		int a = 10;
+		int b = 4;
+		int result = calc.multiply(a, b);
+		assertEquals(40, result);
+	}
+	
+	@Test
+	public void testDivide(){
+		int a = 20;
+		int b = 5;
+		double result = calc.divide(a, b);
+		assertEquals(4, result, 0);
+	}
+}
